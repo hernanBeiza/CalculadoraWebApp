4b363e91052ddd59fbb5beee00b7986ef7337b11
Hernan Beiza <hernan@hiperactivo.cl>
2020-11-04 22:48:41 -0300

    Implementación multiplicación

--- calculadora-web-app/src/App.js
+++ calculadora-web-app/src/App.js
@@ -57,6 +57,14 @@ function App(props) {
       resultadoString = "0";
       */
       break;
+      case "X":
+      if(!primerIngreso){
+        resultado *=parseInt(resultadoString);
+      } else {
+        resultado = parseInt(resultadoString);
+      }
+      resultadoString = "0";
+      break;
     }
     primerIngreso = false;
   }
@@ -133,7 +141,7 @@ function App(props) {
             {/*
             <Button variant="primary" block disabled><FontAwesomeIcon icon={faTimes}/></Button>
             */}
-            <Boton label="X"/>
+            <Boton label="X" funcion={ operar }/>
             </Col>
         </Row>
         <Row>
